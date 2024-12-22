f1fc4dac7303d4ae485e0e1d668753715c6d62af
beyza-yazici <beyzanyazicii@gmail.com>
2024-12-22 16:11:37 +0300

    adding state

--- src/components/currency.jsx
+++ src/components/currency.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 import '../css/currency.css';
+import { useState } from 'react';
+
+let BASE_URL = "https://api.freecurrencyapi.com/v1/latest";
+let API_KEY = "fca_live_kAVeSM40bMxD7OuTABR9D1CbrVft2lFZQenZSORa";
 
 function Currency() {
+
+  const [amount, setAmount] = useState(0);
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("TL");
+  const [result, setResult] = useState(0);
+
+  const exchange = () => {
+    console.log(amount);
+    console.log(fromCurrency);
+    console.log(toCurrency);
+  }
   return (
     <div className="currency-div">
       <div>
@@ -19,8 +34,11 @@ function Currency() {
         </h3>
       </div>
       <div>
-        <input type="number" className="amount" />
-        <select className="from-currency-option">
+        <input 
+        value ={amount}
+        onChange={(e) => setAmount(e.target.value)}
+        type="number" className="amount" />
+        <select onChange={(e) => setFromCurrency(e.target.value)} className="from-currency-option">
           <option>USD</option>
           <option>TL</option>
           <option>EUR</option>
@@ -29,17 +47,22 @@ function Currency() {
 
         <i class="fas fa-arrow-right"></i>
 
-        <select className="to-currency-option">
+        <select onChange={(e) => setToCurrency(e.target.value)} className="to-currency-option">
           <option>TL</option>
           <option>EUR</option>
           <option>USD</option>
           <option>GBP</option>
         </select>
 
-        <input type="number" className="result" />
+        <input 
+        value={result} 
+        onChange={(e) => setResult(e.target.value)}
+        type="number" className="result" />
       </div>
       <div>
-        <button className="exchange-button">Exchange</button>
+        <button 
+        onClick={exchange}
+        className="exchange-button">Exchange</button>
       </div>
     </div>
   );
